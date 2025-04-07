36747f38a9d79ca8eda3023379398514209b67c4
ayush-koli-2003 <ayush28koli31@gmail.com>
2025-04-07 19:52:48 +0530

    report module: improve ui and change generic chart component

--- financial-dashboard-angular/src/app/shared/components/generic-chart/generic-chart.component.ts
+++ financial-dashboard-angular/src/app/shared/components/generic-chart/generic-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ChartType, ChartTypeRegistry } from 'chart.js';
 
 
@@ -8,18 +8,78 @@ import { ChartType, ChartTypeRegistry } from 'chart.js';
   templateUrl: './generic-chart.component.html',
   styleUrl: './generic-chart.component.css'
 })
-export class GenericChartComponent implements OnChanges{
-  @Input() data:any[]=[];
-  @Input() labels:any[]=[];
-  @Input() options:any;
-  @Input() legend:boolean=false;
-  @Input() chartType!:ChartType;
+export class GenericChartComponent implements OnChanges, OnInit {
+  @Input() data: any[] = [];
+  @Input() labels: any[] = [];
+  @Input() options: any;
+  @Input() legend: boolean = false;
+  @Input() chartType!: ChartType;
+  
+  chartOptions: any;
 
-  ngOnInit(){
-    
+  ngOnInit() {
+    this.setDefaultOptions();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    if (changes['options'] || changes['chartType']) {
+      this.setDefaultOptions();
+    }
+  }
 
+  isPieOrDoughnut(): boolean {
+    return this.chartType === 'pie' || this.chartType === 'doughnut';
+  }
+
+  isBarChart(): boolean {
+    return this.chartType === 'bar' || this.chartType === 'horizontalBar';
+  }
+
+  isLineChart(): boolean {
+    return this.chartType === 'line';
+  }
+
+  private setDefaultOptions(): void {
+    const defaultOptions = {
+      responsive: true,
+      maintainAspectRatio: false
+    };
+    
+    if (this.isPieOrDoughnut()) {
+      const pieOptions = {
+        ...defaultOptions,
+        plugins: {
+          legend: {
+            position: 'top',
+          }
+        }
+      };
+      this.chartOptions = this.options ? { ...pieOptions, ...this.options } : pieOptions;
+    } 
+    else if (this.isBarChart()) {
+      const barOptions = {
+        ...defaultOptions,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      };
+      this.chartOptions = this.options ? { ...barOptions, ...this.options } : barOptions;
+    }
+    else if (this.isLineChart()) {
+      const lineOptions = {
+        ...defaultOptions,
+        elements: {
+          line: {
+            tension: 0.4
+          }
+        }
+      };
+      this.chartOptions = this.options ? { ...lineOptions, ...this.options } : lineOptions;
+    }
+    else {
+      this.chartOptions = this.options ? { ...defaultOptions, ...this.options } : defaultOptions;
+    }
   }
 }
